06d34bc96d024c259fa3ca125db5b638200213e2
david cassuto <cassuto.david770@gmail.com>
2020-07-28 19:18:53 +0300

    first commit after build

---
+++ build/precache-manifest.80ddc1beea6e22c9c4e67873d577b368.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b441ec3a486b07362cef1505682e08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52bfed721e186daad5bb",
+    "url": "/static/js/2.6769e8ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6769e8ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9266d599fa9f6a021f1d",
+    "url": "/static/js/main.8368dd03.chunk.js"
+  },
+  {
+    "revision": "93cc3d93768b6259f852",
+    "url": "/static/js/runtime-main.eda82fcf.js"
+  }
+]);
